57cc6c466abfc58b462375685bcf1dffb78ccf3e
Peter Oak <leite.carvalho@gmail.com>
2012-06-24 18:54:31 +0100

    readable zip filename

--- backup.php
+++ backup.php
@@ -803,7 +803,11 @@ class Backup {
         // We might need a lot of memory for this
         @ini_set('memory_limit', apply_filters('admin_memory_limit', WP_MAX_MEMORY_LIMIT));
 
-        $file_name = $this->time . '.zip';
+
+				// add domain name and turn time into readable date, but leave time do create unique file
+				$readabletime = utf8_encode(strftime("%Y-%m-%d",  $this->time ));
+        $file_name = $_SERVER['SERVER_NAME'] .'-'. $readabletime . '-' . $this->time . '.zip';
+        
         $file_path = $this->local_folder . '/' . $file_name;
         
         // Create database dump sql file.
